5bcab3e83172e1beb814ad04c71d926f6e9f9a74
Mike Petras <mpetras@mpre.homes>
2025-06-17 17:26:33 -0600

    Fix: wrap useSearchParams in client-only component

--- src/app/leads/page.tsx
+++ src/app/leads/page.tsx
@@ -1,17 +1,10 @@
-// This must be before "use client" to work
-export const dynamic = 'force-dynamic'
-
 'use client'
 
-import { Suspense } from 'react'
 import LeadsContent from './LeadsContent'
 
 export default function LeadsPage() {
-  return (
-    <Suspense fallback={<div>Loading leads...</div>}>
-      <LeadsContent />
-    </Suspense>
-  )
+  return <LeadsContent />
 }
+
 
 
