0b1eeb5347cb035f5f55528358af3b407a783677
Mike Petras <mpetras@mpre.homes>
2025-06-17 18:24:49 -0600

    Fix: wrap LeadsClient in Suspense to support useSearchParams

--- src/app/leads/page.tsx
+++ src/app/leads/page.tsx
@@ -1,5 +1,14 @@
 'use client'
 
+import { Suspense } from 'react'
 import LeadsClient from './LeadsClient'
-export default LeadsClient
+
+export default function LeadsPage() {
+  return (
+    <Suspense fallback={<div>Loading leads...</div>}>
+      <LeadsClient />
+    </Suspense>
+  )
+}
+
 
