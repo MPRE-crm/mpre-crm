e3cb324681e68f5227d08dedfba2b079f8b000c5
Mike Petras <mpetras@mpre.homes>
2025-06-19 02:00:09 -0600

    Removed invalid serverExternalPackages key from next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,9 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // If using Twilio and other external packages
-  serverExternalPackages: ['twilio'],
-  
   // Optionally, enabling React strict mode (not necessary but helps with debugging)
   reactStrictMode: true,
 };
 
 module.exports = nextConfig;
+
